97840e4dd4d8e0fc0b6f495c85723d3eb155e22d
PushBot <Fake@email.com>
2023-01-01 09:35:01 +0800

    [脚本]自动上传于 2023年01月01日 09:35

---
+++ spush23.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#datime=$(date "+%Y-%m-%d %H:%M")
+datime=$(date "+%Y年%m月%d日 %H:%M")
+cd ~/S1PlainTextArchive2023
+git add .
+git commit -m "[脚本]自动上传于 $datime"
+echo "git commit: 上传于 $datime"
+
